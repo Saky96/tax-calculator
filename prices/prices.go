54837672ed5029d1f9bc84e4b2de0f20d7a39180
Saksham Saigal <saigalsaksham96@gmail.com>
2025-06-16 02:57:46 +0530

    SAKY-306 : added code for writing JSON to a file

--- prices/prices.go
+++ prices/prices.go
@@ -5,32 +5,44 @@ import (
 	"github.com/Saky96/tax-calculator/utils"
 )
 
+type MapOfStringAndString map[string]string
+
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
-	TaxIncludedPrices map[string]float64
+	TaxIncludedPrices MapOfStringAndString
 }
 
 func (job *TaxIncludedPriceJob) LoadData() {
 
 	lines, err := utils.FileManager("prices.txt")
-
 	prices, err := utils.StingsToFloats(lines)
+
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
+
 	job.InputPrices = prices
 }
 
 func (job *TaxIncludedPriceJob) Process() {
 	job.LoadData()
-	result := make(map[string]string)
+	result := make(MapOfStringAndString)
 	for _, price := range job.InputPrices {
 		taxCalculatedPrices := fmt.Sprintf("%.2f", price*(1+job.TaxRate))
 		result[fmt.Sprintf("%.2f", price)] = taxCalculatedPrices
 	}
+
 	fmt.Println(result)
+
+	job.TaxIncludedPrices = result
+
+	err := utils.WriteJSON(job, fmt.Sprintf("results/result_%.0f.json", job.TaxRate*100))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 }
 
 func NewTaxIncludedPriceJob(taxRate float64) *TaxIncludedPriceJob {
--- utils/fileManager.go
+++ utils/fileManager.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"bufio"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
@@ -33,4 +34,23 @@ func FileManager(fileName string) ([]string, error) {
 		return nil, errors.New("error closing file")
 	}
 	return lines, nil
+}
+
+func WriteJSON(data any, path string) error {
+	file, err := os.Create(path)
+	if err != nil {
+		fmt.Println(err)
+		return errors.New("error creating file")
+	}
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	err = encoder.Encode(data)
+	if err != nil {
+		return errors.New("error writing to file")
+	}
+	err = file.Close()
+	if err != nil {
+		return errors.New("error closing file")
+	}
+	return nil
 }

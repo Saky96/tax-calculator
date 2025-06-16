2205b07686bd75bf26590a06349674892203a327
Saksham Saigal <saigalsaksham96@gmail.com>
2025-06-16 20:11:09 +0530

    SAKY-301 : Added code to defer file close

--- utils/fileManager.go
+++ utils/fileManager.go
@@ -28,6 +28,14 @@ func (fm FileManager) ReadFile() ([]string, error) {
 		return nil, errors.New("error opening file")
 	}
 
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println(err)
+			// Handle the error if needed, but we can't return it here since we're in a deferred function
+		}
+	}(file)
+
 	scanner := bufio.NewScanner(file)
 
 	var lines []string
@@ -42,10 +50,10 @@ func (fm FileManager) ReadFile() ([]string, error) {
 		return nil, errors.New("error reading file")
 	}
 
-	err = file.Close()
-	if err != nil {
-		return nil, errors.New("error closing file")
-	}
+	//err = file.Close()
+	//if err != nil {
+	//	return nil, errors.New("error closing file")
+	//}
 	return lines, nil
 }
 
@@ -58,15 +66,23 @@ func (fm FileManager) WriteFile(data any) error {
 
 	time.Sleep(2 * time.Second) // Simulate some delay for demonstration purposes
 
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			fmt.Println(err)
+			// Handle the error if needed, but we can't return it here since we're in a deferred function
+		}
+	}(file)
+
 	encoder := json.NewEncoder(file)
 	encoder.SetIndent("", "  ")
 	err = encoder.Encode(data)
 	if err != nil {
 		return errors.New("error writing to file")
 	}
-	err = file.Close()
-	if err != nil {
-		return errors.New("error closing file")
-	}
+	//err = file.Close()
+	//if err != nil {
+	//	return errors.New("error closing file")
+	//}
 	return nil
 }
